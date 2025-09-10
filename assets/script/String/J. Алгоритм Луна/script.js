a765150a6af99b7b71972dfa6aae7a674f671f7c
I3ravad0 <mihailjmacalister@gmail.com>
2025-09-10 22:48:08 +0300

    [25w37b] String - Luna Algorithm

---
+++ assets/script/String/J. Алгоритм Луна/script.js
@@ -0,0 +1,23 @@
+function checkNumberByLunaAlg(cardNumber) {
+    const preparationArray = cardNumber.split(' ')
+    const numbersArray = []
+    preparationArray.forEach(item => {
+        item.split('').forEach(elem => {
+            numbersArray.push(elem)
+        })
+    });
+    let summ = 0
+    for (let i = 0; i < numbersArray.length; i++) {
+        if (i % 2 === 0) {
+            numbersArray[i] *= 2;
+            if (numbersArray[i] > 9) {
+                numbersArray[i] -= 9
+            } 
+        }
+        summ += +numbersArray[i]
+    }
+
+    return summ % 10 == 0 ? 'Correct' : 'Incorrect' 
+}
+
+console.log(checkNumberByLunaAlg('4561 2612 1234 5467'));
