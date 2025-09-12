e53c0861783d3c934cea71a0ddbf78607bb42d6d
I3ravad0 <mihailjmacalister@gmail.com>
2025-09-13 01:03:38 +0300

    [25w37e] Add code load to console by chosing options

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const selectType = document.querySelector('#type')
 const arraysSelect = document.querySelector('#arrays-tasks')
 const numbersSelect = document.querySelector('#numbers-tasks')
 const stringSelect = document.querySelector('#string-tasks')
+let currentType = ''
 
 selectType.addEventListener('change', (e) => {
     switch (selectType.value) {
@@ -29,3 +30,35 @@ selectType.addEventListener('change', (e) => {
             break;
     }
 })
+
+// LOAD CURRENT SCRIPT
+
+
+function loadTaskScript() {
+    const scriptBox = document.querySelector('#task-script')
+    const scriptTag = document.createElement('script')
+
+    switch (selectType.value) {
+        case 'Arrays':
+            scriptTag.src = `assets/script/${selectType.value}/${arraysSelect.value}/script.js`
+            break;
+
+        case 'Numbers':
+            scriptTag.src = `assets/script/${selectType.value}/${numbersSelect.value}/script.js`
+            break;
+
+        case 'String':
+            scriptTag.src = `assets/script/${selectType.value}/${stringSelect.value}/script.js`
+            break;
+    
+        default:
+            console.log('Error');
+            break;
+    }
+    console.log(scriptTag.src);
+
+    scriptBox.appendChild(scriptTag)
+
+    scriptBox.removeChild(scriptBox.firstChild)
+
+}
